88258f1553754c9bf9168dbbf67f5712f2f187c2
Paulo Vinicius <ploviniciusplo@gmail.com>
2021-01-27 00:22:33 -0300

    feat(section3): Business logic lesson

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,21 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
-    public function show($post) {
-        $posts = [
-            'my-first-post' => 'Hello, this is my first blog post!',
-            'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-        ];
-
-        if ( !array_key_exists($post, $posts)) {
-            abort(404, 'Sorry, that post was not found.');
-        }
+    public function show($slug) {
+        $post = Post::where('slug', $slug)->firstOrFail();
 
         return view('post', [
-            'post' => $posts[$post]
+            'post' => $post
         ]);
     }
 }
